cb2af2a557dc51ba8ea93eae25573b48994cb408
Alexandra Glen <varyandis@yandex.ru>
2024-11-06 11:28:55 +0300

    feat: add 13-14 functions

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -232,8 +232,8 @@ function startsWith(str, substr) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
@@ -249,8 +249,10 @@ function endsWith(/* str, substr */) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const min = String(minutes).padStart(2, 0);
+  const sec = String(seconds).padStart(2, 0);
+  return `${min}:${sec}`;
 }
 
 /**
